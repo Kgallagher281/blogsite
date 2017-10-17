c024bd190447dbfe65be504c8332f5aeed078e07
Kevin <kevin.gallagher23@gmail.com>
2017-10-17 17:11:55 -0400

    backend, seeds, app, model,

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
+require './models'
 
 set :database, {adapter: 'sqlite3', database: 'blogsite.sqlite3'}
+
 get '/' do
   erb :blog
-  
 end
 
 get '/post' do
@@ -22,4 +23,17 @@ end
 
 get'/contact' do
 	erb :contact
+end
+
+post '/signin' do
+user = User.find_by(email: params[:email])
+if user && user.password == params[:password]
+	session[:user_id] = user.id
+	flash[:message] = "welcome to BlogHub"
+	redirect '/'
+
+else flash[:message] = "Wrong username or password"
+	redirect back
+end
+
 end
